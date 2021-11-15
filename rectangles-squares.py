cd06cc30b75898824d9356e9ef20d6fe8420b748
Khasanov Islam <islam.khasanov.99@mailru>
2021-11-15 11:52:11 +0500

    Added 6 laboratories.

---
+++ rectangles-squares.py
@@ -0,0 +1,51 @@
+"""
+rectangle-squares.py
+Import the "star-geometry" module and use it.
+
+Warning!
+The program only checks for the existing of the input file,
+but doesn't check the data itself.
+
+(c) Хасанов Ислам > КЭ-101
+"""
+from star_geometry import PrintSquare, PrintRectangle
+
+
+if __name__ == '__main__':
+    match input(f"  1 - PrintRectangle\n"
+                f"  2 - PrintSquare\n"
+                f"Enter the number: "):
+        case "1":
+            print("Selected PrintRectangle")
+            print("Reading data from 'input.txt'...")
+            try:
+                with open("input.txt", "r") as data:
+                    rectangle_size = tuple(map(int, data.readline().split()))
+                    print(f"Rectangle size: {rectangle_size[0]}x{rectangle_size[1]}")
+
+                    file_to_write = data.readline()
+                    file_to_write = file_to_write.rstrip()
+                    print(f"File name to write: {file_to_write}")
+
+                    PrintRectangle(*rectangle_size, file_to_write)
+                    print(" Done!")
+            except FileNotFoundError:
+                print("'input.txt' file not found.\n Termination...")
+        case "2":
+            try:
+                print("Selected PrintSquare")
+                print("Reading data from 'input.txt'...")
+                with open("input.txt", 'r') as data:
+                    square_size = int(data.readline())
+                    print(f"Square size: {square_size}x{square_size}")
+
+                    file_to_write = data.readline()
+                    file_to_write = file_to_write.rstrip()
+                    print(f"File name to write: {file_to_write}")
+
+                    PrintSquare(square_size, file_to_write)
+                    print(" Done!")
+            except FileNotFoundError:
+                print("'input.txt' file not found.\n Termination...")
+        case _:
+            print("Nothing is selected.\n Termination...")
