58273b1dc7d50889a3fc676db885eec59eb4d231
Khasanov41 <islam.khasanov.99@mail.ru>
2021-12-11 21:45:26 +0500

    lab7-extra minor refactoring.

--- lab7-extra/core.py
+++ lab7-extra/core.py
@@ -4,15 +4,60 @@ def read_input(file="input.txt"):
         return data
 
 
+def get_output(data):
+    """
+    Creates a table based on a two-dimensional array with string data.
+    Each row of the array is a row of the table. The first line is the title.
+    All array strings must be of the same length (they can be complemented with empty strings "").
+
+    :param list | tuple data: Two-dimensional array with string data
+    :return string: String in the form of a table.
+    """
+    if data is None:
+        return "Маршрут не существует"
+    else:
+        size = (len(data), len(data[0]))
+        # Getting the maximum width of each column.
+        width = [0 for _ in range(len(data[0]))]
+        for col in range(size[1]):
+            for row in range(size[0]):
+                width[col] = max(width[col], len(data[row][col])+2)
+        # Header separator.
+        sep = '+'+'-'*(sum(width)+len(width)-1)+'+\n'
+
+        # Adds a table header.
+        table = sep
+        for col in range(size[1]):
+            table += "|"+data[0][col].center(width[col])
+        else:
+            table += "|\n"+sep
+
+        # Adds the body of the table.
+        for row in range(1, size[0]):
+            for col in range(size[1]):
+                table += "|"+data[row][col].center(width[col])
+            table += "|\n"
+        else:
+            table += sep
+
+        return table
+
+
 def check_move(field, pos, shape):
     return (0 <= pos[0] < shape[0]) \
             and (0 <= pos[1] < shape[1]) \
             and (field[pos[1]][pos[0]] == 0)
 
 
+def print_field(field):
+    for row in field:
+        print(row)
+    print()
+
+
 def check_field(field):
     for row in field:
-        if 0 in row:
+        if 0 in row or len(field) == 1:
             return False
     return True
 
@@ -28,28 +73,36 @@ def count_move(field, pos, shape, moves):
 
 def take_path(field, pos, shape, moves, step=1):
     field[pos[1]][pos[0]] = step
-    rate = 0
+    next_pos = None
+    rate = 9
     for move in moves:
         next_move = pos[0]+move[0], pos[1]+move[1]
         if check_move(field, next_move, shape):
             cur_rate = count_move(field, next_move, shape, moves)
-            if cur_rate > rate:
+            if cur_rate < rate:
                 next_pos = next_move
                 rate = cur_rate
         else:
             continue
-    if rate == 0 and check_field(field):
+    if rate == 9 and not check_field(field):
         return None
-    elif rate == 0:
+    elif rate == 9:
         return field
     else:
         return take_path(field, next_pos, shape, moves, step+1)
 
 
+def _test_result(shape, moves):
+    for x in range(shape[0]):
+        for y in range(shape[1]):
+            field = [[0] * shape[0] for _ in range(shape[1])]
+            result = take_path(field, (x, y), shape, moves)
+            if result is not None:
+                print(x, y)
+                print_field(result)
+
 
 if __name__ == "__main__":
-    sides = (8, 8)
-    position = (4, 4)
-    board = [[0] * sides[0]] * sides[1]
+    sides = (10, 10)
     horse_moves = ((-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1))
-    # print(count_move(board, position, sides, horse_moves))
+    _test_result(sides, horse_moves)
--- lab7-extra/main.py
+++ lab7-extra/main.py
@@ -1,14 +1,20 @@
-from core import read_input, find_next_move
+"""
+!!! BUG WITH COORDINATES !!!
+"""
+import core
 
 
 if __name__ == "__main__":
-    inp = "M=10, N=10, X=1, Y=1"    # read_input()
+    inp = "M=10, N=10, X=9, Y=9"    # core.read_input()
 
     # Parsing input data.
     trans_table = str.maketrans({'M': '', 'N': '', 'X': '', 'Y': '', ',': '', '=': ''})
     inp = tuple(map(int, inp.translate(trans_table).split()))
-    sides, x_y = inp[0:2], inp[2:4]
+    sides, position = inp[0:2], inp[2:4]
 
-    board = [[0] * x_y[0]] * x_y[1]
-    moves = ((-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1))
+    board = [[0] * sides[0] for _ in range(sides[1])]
+    horse_moves = ((-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1))
 
+    path = core.take_path(board, position, sides, horse_moves)
+    result = core.get_output(path)
+    print(result)
