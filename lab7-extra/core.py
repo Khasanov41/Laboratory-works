20b15e9d1622483a6db2bad2a1148b3e66091850
Khasanov41 <islam.khasanov.99@mail.ru>
2021-12-13 23:18:06 +0500

    Completion of the lab7-extra.

--- lab7-extra/core.py
+++ lab7-extra/core.py
@@ -1,95 +1,118 @@
 def read_input(file="input.txt"):
-    with open(file, "r") as file:
-        data = file.read().strip()
+    with open(file, "r") as txt:
+        data = txt.read().strip()
         return data
 
 
-def get_output(data):
+def write_output(data, file="output.txt"):
+    with open(file, 'w') as txt:
+        txt.write(data + "\n")
+
+
+def take_path(field, pos, shape, moves, step=1):
+    """
+    Recursively creates a matrix with the route of the chess board moves,
+    by applying Warnsdorff's rule. If the chessboard cannot be filled
+    with moves, then None is returned.
+
+    :param list field: Two - dimensional matrix (board).
+    :param list | tuple pos: The positions of the figure, with (1, 1) being the lower-left corner of the board.
+    :param list | tuple shape: The width and height of the board.
+    :param list | tuple moves: A set of moves of the figure.
+    :param int step: The number of steps the figure has taken. Don't change manually!
+    :return: None or two-dimensional matrix with figure's moves.
+    """
+    field[pos[1]][pos[0]] = str(step)  # Mark the cell with the current move number.
+    next_pos = None
+    rate = len(moves)  # The number of maximum possible moves from a given position.
+    for move in moves:
+        # Searches for the position with the lowest rate.
+        next_move = pos[0]+move[0], pos[1]+move[1]
+        if _check_move(field, next_move, shape):
+            cur_rate = _count_move(field, next_move, shape, moves)
+            if cur_rate < rate:
+                next_pos = next_move
+                rate = cur_rate
+        else:
+            continue
+    if rate == len(moves) and not _check_field(field):
+        return None  # Unable to find a route.
+    elif rate == len(moves):
+        return field  # Route found.
+    else:
+        # Search next move.
+        return take_path(field, next_pos, shape, moves, step+1)
+
+
+def make_table(data):
     """
     Creates a table based on a two-dimensional array with string data.
-    Each row of the array is a row of the table. The first line is the title.
-    All array strings must be of the same length (they can be complemented with empty strings "").
+    Each row of the array is a row of the table. All array strings
+    must be of the same length (they can be complemented with empty strings "").
 
-    :param list | tuple data: Two-dimensional array with string data
-    :return string: String in the form of a table.
+    :param list | tuple data: Two-dimensional array with string data.
+    :return: String in the form of a table.
     """
-    if data is None:
-        return "Маршрут не существует"
+    size = (len(data), len(data[0]))
+    # Getting the maximum width of each column.
+    width = [0 for _ in range(len(data[0]))]
+    for col in range(size[1]):
+        for row in range(size[0]):
+            width[col] = max(width[col], len(data[row][col])+2)
+    # Header separator.
+    sep = '+'+'-'*(sum(width)+len(width)-1)+'+\n'
+    # Adds a table header.
+    table = sep
+    for col in range(size[1]):
+        table += "|"+data[0][col].center(width[col])
     else:
-        size = (len(data), len(data[0]))
-        # Getting the maximum width of each column.
-        width = [0 for _ in range(len(data[0]))]
+        table += "|\n"+sep
+    # Adds the body of the table.
+    for row in range(1, size[0]):
         for col in range(size[1]):
-            for row in range(size[0]):
-                width[col] = max(width[col], len(data[row][col])+2)
-        # Header separator.
-        sep = '+'+'-'*(sum(width)+len(width)-1)+'+\n'
+            table += "|"+data[row][col].center(width[col])
+        table += "|\n" + sep
 
-        # Adds a table header.
-        table = sep
-        for col in range(size[1]):
-            table += "|"+data[0][col].center(width[col])
-        else:
-            table += "|\n"+sep
-
-        # Adds the body of the table.
-        for row in range(1, size[0]):
-            for col in range(size[1]):
-                table += "|"+data[row][col].center(width[col])
-            table += "|\n"
-        else:
-            table += sep
-
-        return table
+    return table
 
 
-def check_move(field, pos, shape):
+def convert_pos(pos, shape):
+    """
+    Translates the position to the index of the matrix.
+
+    :param tuple | list pos: Abscissa and ordinate.
+    :param tuple | list shape: The width and height of the field.
+    :return: The index of a two-dimensional matrix.
+    """
+    return pos[0]-1, shape[1]-pos[1]
+
+
+def _check_move(field, pos, shape):
     return (0 <= pos[0] < shape[0]) \
             and (0 <= pos[1] < shape[1]) \
             and (field[pos[1]][pos[0]] == 0)
 
 
-def print_field(field):
+def _print_field(field):
     for row in field:
         print(row)
     print()
 
 
-def check_field(field):
+def _check_field(field):
     for row in field:
         if 0 in row or len(field) == 1:
             return False
     return True
 
 
-def count_move(field, pos, shape, moves):
+def _count_move(field, pos, shape, moves):
     count = 0
     for move in moves:
         next_move = pos[0]+move[0], pos[1]+move[1]
-        if check_move(field, next_move, shape):
+        if _check_move(field, next_move, shape):
             count += 1
     return count
-
-
-def take_path(field, pos, shape, moves, step=1):
-    field[pos[1]][pos[0]] = step
-    next_pos = None
-    rate = 9
-    for move in moves:
-        next_move = pos[0]+move[0], pos[1]+move[1]
-        if check_move(field, next_move, shape):
-            cur_rate = count_move(field, next_move, shape, moves)
-            if cur_rate < rate:
-                next_pos = next_move
-                rate = cur_rate
-        else:
-            continue
-    if rate == 9 and not check_field(field):
-        return None
-    elif rate == 9:
-        return field
-    else:
-        return take_path(field, next_pos, shape, moves, step+1)
 
 
 def _test_result(shape, moves):
@@ -99,10 +122,4 @@ def _test_result(shape, moves):
             result = take_path(field, (x, y), shape, moves)
             if result is not None:
                 print(x, y)
-                print_field(result)
-
-
-if __name__ == "__main__":
-    sides = (10, 10)
-    horse_moves = ((-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1))
-    _test_result(sides, horse_moves)
+                _print_field(result)
--- lab7-extra/main.py
+++ lab7-extra/main.py
@@ -1,20 +1,34 @@
 """
-!!! BUG WITH COORDINATES !!!
+main.py
+
+Находит маршрут обхода конем шахматной доски, заданных размеров, из заданного начального положения коня.
+Входные данные (файл input.txt):
+    M, N - размеры шахматной доски.
+    X, Y - начальные координаты расположения коня.
+Выходные данные (файл output.txt):
+    Таблица с номером хода в каждой ячейки поля, если маршрут существует
+    и "Маршрут не существует" в противном случае.
+Пример входных данных:
+    M=5 N=5 X=1 Y=1
+
+(c) Хасанов Ислам, КЭ-101
+GitHub: https://github.com/Khasanov41/Laboratory-works.git
 """
 import core
 
 
 if __name__ == "__main__":
-    inp = "M=10, N=10, X=9, Y=9"    # core.read_input()
+    inp = core.read_input()  # Use "M=10, N=10, X=10, Y=10" for test.
 
     # Parsing input data.
     trans_table = str.maketrans({'M': '', 'N': '', 'X': '', 'Y': '', ',': '', '=': ''})
     inp = tuple(map(int, inp.translate(trans_table).split()))
     sides, position = inp[0:2], inp[2:4]
 
+    position = core.convert_pos(position, sides)
     board = [[0] * sides[0] for _ in range(sides[1])]
     horse_moves = ((-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1))
 
-    path = core.take_path(board, position, sides, horse_moves)
-    result = core.get_output(path)
-    print(result)
+    route = core.take_path(board, position, sides, horse_moves)
+    table = "Маршрут не существует" if route is None else core.make_table(route)
+    core.write_output(table)
