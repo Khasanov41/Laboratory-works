6731a6937dfb80b36dcbf16b6926e48f733a10bd
Khasanov41 <islam.khasanov.99@mail.ru>
2021-12-20 22:29:46 +0500

    Add sort.py with sort functions.

--- lab9/tape.py
+++ lab9/tape.py
@@ -1,47 +1,66 @@
-import os, os.path
+import os
+import os.path
 from contextlib import contextmanager
 
 
+def segments(source, limit):
+	segment = []
+	for line in open(source, 'r'):
+		e = int(line.strip())
+		segment.append(e)
+		if len(segment) == limit:
+			yield segment
+			segment = []
+	else:
+		yield segment
+
+
 @contextmanager
-def tapes(number):
-	
+def tapes(number, seq_len, segment=(0, 999)):
+	"""
+	Генерирует ленты для работы внешней сортировки в папке ./tapes вида 0.tape, 1.tape, ..., <number>.tape.
+	исходной последовательностью, которую необходимо отсортироват.
+	После работы очищает директорию ./tapes и сохраняет результат в result.tape.
+	Работа с лентами будет невозможна без вызова этой функции
 
-
-
-def gen_source(leng, start=0, stop=999):
+	:param segment:
+	:param seq_len:
+	:param int number: Количество лент
+	:return:
+	"""
 	from random import randint
-
+	# from shutil import rmtree
+	if not os.path.exists(_dir):
+		os.mkdir(_dir)
 	try:
-		if not os.path.exists(dir):
-			os.mkdir(dir)
-		source = open(dir + "0.tape", 'w')
-		source.write(" ".join(str(randint(start, stop)) for _ in range(leng)))
-		source.write("\n")
-		return 0
-	except OSError:
-		print("Error: Failed to create a directory with the source!")
-		return 1
+		# Creating source tape
+		with open(_dir + "0.tape", 'w') as source:
+			for _ in range(seq_len):
+				source.write(str(randint(*segment)) + "\n")
+		yield _dir + "0.tape"
+		# Creating empty tapes
+		for i in range(1, number+1):
+			with open(_dir + str(i)+".tape", 'w'):
+				pass
 	finally:
-		source.close()
-
-def gen_tape(number):
-	try:
-		print(dir)
-		for i in range(number):
-			
-		#pass
-	except OSError:
-		print("Error: Failed to create a directory with the source!")
-		return 1
+		print("Directory is deleted.")  # rmtree(_dir)
 
 
-def read_tape(size, files):
-	pass
+def write_seg(num, segment):
+	with open(_dir+str(num)+'.tape', 'a') as tape:
+		for e in segment:
+			tape.write(str(e) + "\n")
 
 
 _dir = os.path.join("tapes/")
 
 
 if __name__ == "__main__":
-	gen_tape(10)
-	gen_source(10)
+	LIMIT = 3
+	FILES = 5
+	SEQ = 20
+	# with tapes(FILES, SEQ) as file:
+	# for seg in segments(file, LIMIT):
+	# print(seg)
+	seg = [1, 2, 3, 4, 5, 6, 7]
+	write_seg(0, seg)
